63023220e1db551453e25b9532b90c41b8e0d04d
BLORT-DESKTOP\Blort <fstein@gmail.com>
2021-12-10 21:54:32 -0300

    EVM server launch and state WIP

---
+++ Assets/Emergence/Scripts/Types/Persona.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Emergence
+{
+    public class Persona
+    {
+        public string id;
+        public string avatar;
+
+        private Texture2D avatarImage = null;
+
+        public Texture2D AvatarImage
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Assets/Emergence/Scripts/Utils/EmergenceState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Emergence
+{
+    public class EmergenceState : MonoBehaviour
+    {
+        public static bool IsConnected
+        {
+            get;
+            private set;
+        }
+
+        public static Persona CurrentPersona
+        {
+            get;
+            private set;
+        }
+    }
+}
