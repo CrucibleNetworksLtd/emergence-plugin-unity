cad9471128c987bbe1301ae07f1bc5f23083e736
BLORT-DESKTOP\Blort <fstein@gmail.com>
2021-12-13 17:14:30 -0300

    Persona class data

--- Assets/Emergence/Scripts/Types/Persona.cs
+++ Assets/Emergence/Scripts/Types/Persona.cs
@@ -4,8 +4,18 @@ namespace Emergence
 {
     public class Persona
     {
+        public struct PersonaSettings
+        {
+            bool availableOnSearch;
+            bool receiveContactRequest;
+            bool showStatus;
+        }
+
         public string id;
+        public string name;
+        public string bio;
         public string avatar;
+        public PersonaSettings settings;
 
         private Texture2D avatarImage = null;
 
