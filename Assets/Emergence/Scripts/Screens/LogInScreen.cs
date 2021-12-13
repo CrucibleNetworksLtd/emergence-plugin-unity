6b14103b03785dcb8b896d82f6763a9e767facc2
Federico Stein <DrBlort@users.noreply.github.com>
2021-12-13 17:51:45 -0300

    Log in screen (#4)

--- Assets/Emergence/Scripts/Screens/LogInScreen.cs
+++ Assets/Emergence/Scripts/Screens/LogInScreen.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Emergence
 {
     public class LogInScreen : MonoBehaviour
     {
-        private void Start()
-        {
+        [Header("UI References")]
+        public RawImage qrRawTexture;
+        public Image qrImage;
+        public TextMeshProUGUI refreshCounterText;
 
-        }
 
-        private void Update()
-        {
-
-        }
     }
 }
