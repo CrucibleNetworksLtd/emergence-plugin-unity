427a594da41722d69967d9f275ebc6f75fea632b
Federico Stein <DrBlort@users.noreply.github.com>
2021-12-13 17:29:55 -0300

    Ticket/ce 319 (#3)
    * EVM server launch and state WIP
    
    * Persona class data

---
+++ Assets/Emergence/Editor/EmergenceEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EmergenceEditor : Editor
+{
+    [MenuItem("Emergence/Run EVM Server")]
+    static void RunEVMServer()
+    {
+        Debug.Log("Run EVM Server");
+    }
+
+    [MenuItem("Emergence/Stop EVM Server")]
+    static void StopEVMServer()
+    {
+        Debug.Log("Stop EVM Server");
+    }
+
+    [MenuItem("Emergence/Restart EVM Server")]
+    static void RestartEVMServer()
+    {
+        Debug.Log("Restart EVM Server");
+    }
+}
---
+++ Assets/Emergence/Scripts/Types/Persona.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Emergence
+{
+    public class Persona
+    {
+        public struct PersonaSettings
+        {
+            bool availableOnSearch;
+            bool receiveContactRequest;
+            bool showStatus;
+        }
+
+        public string id;
+        public string name;
+        public string bio;
+        public string avatar;
+        public PersonaSettings settings;
+
+        private Texture2D avatarImage = null;
+
+        public Texture2D AvatarImage
+        {
+            get;
+            set;
+        }
+    }
+}
