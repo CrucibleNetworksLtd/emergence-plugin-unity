bb55b1ee45db6dac6d5ddf013bf3bd237dc42e45
BLORT-DESKTOP\Blort <fstein@gmail.com>
2021-12-17 15:55:24 -0300

    Input fields, toggles

--- Assets/Emergence/Scripts/Screens/EditPersonaScreen.cs
+++ Assets/Emergence/Scripts/Screens/EditPersonaScreen.cs
@@ -14,5 +14,63 @@ namespace Emergence
         public Transform avatarScrollRoot;
         public TextMeshProUGUI title;
         public Button createButton;
+
+        public TMP_InputField nameIF;
+        public TMP_InputField bioIF;
+        public RawImage personaAvatar;
+
+        public Toggle availableOnSearchesToggle;
+        public Toggle showingMyStatusToggle;
+        public Toggle receiveContactRequestsToggle;
+
+        private void Awake()
+        {
+            createButton.onClick.AddListener(OnCreateClicked);
+            backButton.onClick.AddListener(OnBackClicked);
+        }
+
+        public void Refresh(Persona persona)
+        {
+            if (persona != null)
+            {
+                nameIF.text = persona.name;
+                bioIF.text = persona.bio;
+                
+                availableOnSearchesToggle.SetIsOnWithoutNotify(persona.settings.availableOnSearch);
+                showingMyStatusToggle.SetIsOnWithoutNotify(persona.settings.showStatus);
+                receiveContactRequestsToggle.SetIsOnWithoutNotify(persona.settings.receiveContactRequest);
+
+                if (persona.AvatarImage)
+                {
+                    personaAvatar.texture = persona.AvatarImage;
+                }
+                 else
+                {
+                    // TODO default image
+                }
+            }
+
+            // Clear scroll area
+            while (avatarScrollRoot.childCount > 0)
+            {
+                GameObject child = avatarScrollRoot.GetChild(0).gameObject;
+                avatarScrollItemsPool.ReturnUsedObject(child);
+            }
+
+            // TODO all avatar images
+            GameObject go = avatarScrollItemsPool.GetNewObject();
+            go.transform.SetParent(avatarScrollRoot);
+            go.transform.localScale = Vector3.one;
+        }
+
+        private void OnCreateClicked()
+        {
+            // TODO Save persona
+        }
+
+        private void OnBackClicked()
+        {
+            EmergenceManager.Instance.ShowDashboard();
+        }
     }
 }
