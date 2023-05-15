6cd4c54a6e9180852171c585f1e881d9d4ac2e3a
thoxey <tom@Toms-MBP.cust.communityfibre.co.uk>
2023-05-15 16:32:58 +0100

    Fixing read method logging

--- Assets/Emergence/Samples/EmergenceDemo/Scripts/DemoStations/ReadMethod.cs
+++ Assets/Emergence/Samples/EmergenceDemo/Scripts/DemoStations/ReadMethod.cs
@@ -1,8 +1,8 @@
+using System.Collections.Generic;
 using EmergenceSDK.Internal.Utils;
 using EmergenceSDK.ScriptableObjects;
 using EmergenceSDK.Services;
 using EmergenceSDK.Types;
-using EmergenceSDK.Types.Responses;
 using UnityEngine;
 
 namespace EmergenceSDK.EmergenceDemo.DemoStations
@@ -54,11 +54,26 @@ namespace EmergenceSDK.EmergenceDemo.DemoStations
                 deployedContract.chain.networkName, OnLoadSuccess, (message, id) => { Debug.LogError("Error while loading contract: " + message); });
         }
 
+        public class ContractResponse
+        {
+            public List<string> response { get; set; }
+
+            public override string ToString()
+            {
+                 return string.Join(", ", response);
+            }
+        }
+        
         private void OnLoadSuccess()
         {
             ContractInfo contractInfo = new ContractInfo(deployedContract.contractAddress, "GetCurrentCount", deployedContract.chain.networkName, deployedContract.chain.DefaultNodeURL);
-            ContractService.ReadMethod<BaseResponse<string>, string[]>(contractInfo, new string[] { EmergenceSingleton.Instance.GetCachedAddress() },
-                (response) => Debug.Log($"ReadContract finished: {response.message}"), EmergenceLogger.LogError);
+            ContractService.ReadMethod<ContractResponse, string[]>(contractInfo, new string[] { EmergenceSingleton.Instance.GetCachedAddress() },
+                ReadMethodSuccess, EmergenceLogger.LogError);
+        }
+
+        private void ReadMethodSuccess(ContractResponse response)
+        {
+            Debug.Log($"ReadContract finished: {response}");
         }
     }
 }
