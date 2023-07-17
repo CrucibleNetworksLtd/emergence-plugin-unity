80f0324d0f661a2d39dfa79593b59b00e3cf1169
thoxey <tom@Toms-MBP.cust.communityfibre.co.uk>
2023-07-17 12:09:04 +0100

    Adding new wallet apis to interface

--- Assets/Emergence/Scripts/Emergence/Services/IWalletService.cs
+++ Assets/Emergence/Scripts/Emergence/Services/IWalletService.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using EmergenceSDK.Types;
 
 namespace EmergenceSDK.Services
 {
@@ -16,21 +17,37 @@ namespace EmergenceSDK.Services
         /// Attempts to sign a message using the walletconnect protocol, the success callback will return the signed message
         /// </summary>
         UniTask RequestToSign(string messageToSign, RequestToSignSuccess success, ErrorCallback errorCallback);
+        /// <summary>
+        /// Attempts to sign a message using the walletconnect protocol.
+        /// </summary>
+        UniTask<ServiceResponse<string>> RequestToSignAsync(string messageToSign);
         
         /// <summary>
         /// Attempts to handshake with the Emergence server, retrieving the wallet address if successful.
         /// </summary>
         UniTask Handshake(HandshakeSuccess success, ErrorCallback errorCallback);
+        /// <summary>
+        /// Attempts to handshake with the Emergence server.
+        /// </summary>
+        UniTask<ServiceResponse<string>> HandshakeAsync();
 
         /// <summary>
         /// Attempts to get the balance of the wallet, the success callback will fire with the balance if successful
         /// </summary>
         UniTask GetBalance(BalanceSuccess success, ErrorCallback errorCallback);
+        /// <summary>
+        /// Attempts to get the balance of the wallet.
+        /// </summary>
+        UniTask<ServiceResponse<string>> GetBalanceAsync();
         
 
         /// <summary>
         /// Attempts to validate a signed message, the success callback will fire with the validation result if the call is successful
         /// </summary>
         UniTask ValidateSignedMessage(string message, string signedMessage, string address, ValidateSignedMessageSuccess success, ErrorCallback errorCallback);
+        /// <summary>
+        /// Attempts to validate a signed message.
+        /// </summary>
+        UniTask<ServiceResponse<bool>> ValidateSignedMessageAsync(ValidateSignedMessageRequest data);
     }
 }
