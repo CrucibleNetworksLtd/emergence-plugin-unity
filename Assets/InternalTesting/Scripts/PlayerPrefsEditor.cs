538c16127051e2d135651924c7d919af69efe357
thoxey <tom@Toms-MBP.cust.communityfibre.co.uk>
2023-10-11 12:05:32 +0100

    Updates to test plan and test utility script

---
+++ Assets/InternalTesting/Scripts/PlayerPrefsEditor.cs
@@ -0,0 +1,28 @@
+using EmergenceSDK.Internal.Utils;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsEditor : EditorWindow
+{
+    [MenuItem("Window/Emergence Internal/PlayerPrefsEditor")]
+    public static void ShowWindow()
+    {
+        GetWindow<PlayerPrefsEditor>("Player Prefs Editor");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("PlayerPrefs Editor", EditorStyles.boldLabel);
+
+        // Display PlayerPrefs keys and values
+        int hasLoggedInOnceValue = PlayerPrefs.GetInt(StaticConfig.HasLoggedInOnceKey, 0);
+        GUILayout.Label($"{StaticConfig.HasLoggedInOnceKey}: {hasLoggedInOnceValue}");
+
+        // Add a button to reset the HasLoggedInOnceKey value to 0
+        if (GUILayout.Button("Reset HasLoggedInOnceKey"))
+        {
+            PlayerPrefs.SetInt(StaticConfig.HasLoggedInOnceKey, 0);
+            PlayerPrefs.Save();
+        }
+    }
+}
