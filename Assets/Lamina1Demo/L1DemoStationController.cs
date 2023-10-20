4f43f4763efa07f6e9c57a301b83fef660d6ed11
thoxey <tom@Toms-MBP.cust.communityfibre.co.uk>
2023-10-20 14:47:14 +0100

    Adding L1 Demo scene

---
+++ Assets/Lamina1Demo/L1DemoStationController.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using EmergenceSDK.Internal.Utils;
+using EmergenceSDK.Services;
+using UnityEngine;
+
+namespace EmergenceSDK.EmergenceDemo.DemoStations
+{
+    public class L1DemoStationController : MonoBehaviour
+    {
+        private bool IsLoggedIn() => personaService.CurrentAccessToken.Length != 0;
+        
+        public DemoStation<OpenOverlay> openOverlay;
+        public DemoStation<MintAvatar> mintAvatar;
+        public DemoStation<L1MintStation> l1MintStation;
+
+        private List<IDemoStation> stationsRequiringLogin;
+        private IPersonaService personaService;
+
+        public async void Awake()
+        {
+            stationsRequiringLogin = new List<IDemoStation>()
+            {
+                mintAvatar as IDemoStation,
+                l1MintStation as IDemoStation
+            };
+
+            //OpenOverlay is the first station, so we can set it to ready here
+            (openOverlay as IDemoStation).IsReady = true;
+            
+            await UniTask.WaitUntil(IsLoggedIn);
+            ActivateStations();
+        }
+
+        public void Start()
+        {
+            personaService = EmergenceServices.GetService<IPersonaService>();
+        }
+
+        private void ActivateStations()
+        {
+            EmergenceLogger.LogInfo("Activating stations", true);
+            foreach (var station in stationsRequiringLogin)
+            {
+                station.IsReady = true;
+            }
+        }
+    }
+}
---
+++ Assets/Lamina1Demo/L1MintStation.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using EmergenceSDK.EmergenceDemo.DemoStations;
+using EmergenceSDK.Internal.Utils;
+using EmergenceSDK.ScriptableObjects;
+using EmergenceSDK.Services;
+using EmergenceSDK.Types.Responses;
+using UnityEngine;
+
+namespace EmergenceSDK
+{
+    public class L1MintStation : DemoStation<L1MintStation>, IDemoStation
+    {
+        public DeployedSmartContract deployedContract;
+
+        public bool IsReady
+        {
+            get => isReady;
+            set
+            {
+                InstructionsText.text = value ? ActiveInstructions : InactiveInstructions;
+                isReady = value;
+            }
+        }
+        
+        private IContractService ContractService => contractService ??= EmergenceServices.GetService<IContractService>();
+        private IContractService contractService;
+
+        private void Start()
+        {
+            instructionsGO.SetActive(false);
+            IsReady = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            instructionsGO.SetActive(true);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            instructionsGO.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (HasBeenActivated() && IsReady)
+            {
+                MintL1Token();
+            }
+        }
+
+        private void MintL1Token()
+        {
+            
+            var contractInfo = new ContractInfo(deployedContract, "mint");
+            
+            ContractService.WriteMethod(contractInfo, "", "", "0", new string[] { }, OnWriteSuccess, EmergenceLogger.LogError);
+        }
+        
+        private void OnWriteSuccess(BaseResponse<string> response)
+        {
+            EmergenceLogger.LogInfo("Mint called successfully", true);
+        }
+    }
+}
