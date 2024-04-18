712973e95b7160396f618b6eb993ca463e136efe
Guido Belluomo <156204138+CrucibleGuido@users.noreply.github.com>
2024-04-18 16:15:08 +0200

    WalletConnect 2.0 server

--- Assets/Emergence/Scripts/Internal/Utils/StaticConfig.cs
+++ Assets/Emergence/Scripts/Internal/Utils/StaticConfig.cs
@@ -6,7 +6,7 @@ namespace EmergenceSDK.Internal.Utils
         /// URL pointing to the Emergence API.
         /// <remarks>Changing this will break Emergence</remarks>
         /// </summary>
-        public const string APIBase = "https://evm3.openmeta.xyz/api/";
+        public const string APIBase = "https://cloudevmv2.openmeta.xyz/IntegrationLibrary/";
 
         public const string HasLoggedInOnceKey = "HasLoggedInOnce";
     }
