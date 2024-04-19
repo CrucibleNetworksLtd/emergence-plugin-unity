b79e9a35e90a1fc1c959c522ea2582ea7de108e9
Guido Belluomo <156204138+CrucibleGuido@users.noreply.github.com>
2024-04-19 20:23:58 +0200

    Added methods to spoof wallet address for testing

--- Assets/EmergenceSDK/Integrations/Futureverse/Internal/Services/IWalletServiceInternal.cs
+++ Assets/EmergenceSDK/Integrations/Futureverse/Internal/Services/IWalletServiceInternal.cs
@@ -1,4 +1,5 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using Cysharp.Threading.Tasks;
 using EmergenceSDK.Services;
 using EmergenceSDK.Types;
 using EmergenceSDK.Types.Delegates;
@@ -15,5 +16,7 @@ namespace EmergenceSDK.Integrations.Futureverse.Internal.Services
         /// Attempts to handshake with the Emergence server.
         /// </summary>
         UniTask<ServiceResponse<string>> HandshakeAsync();
+        void RunWithSpoofedWalletAddress(string walletAddress, string checksummedWalletAddress, Action action);
+        UniTask RunWithSpoofedWalletAddressAsync(string walletAddress, string checksummedWalletAddress, Func<UniTask> action);
     }
 }
