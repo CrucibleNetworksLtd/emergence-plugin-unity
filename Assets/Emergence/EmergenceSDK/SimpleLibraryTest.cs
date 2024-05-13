877e8c154e7cde271bde3f2351f97d416ff49a4a
Guido Belluomo <156204138+CrucibleGuido@users.noreply.github.com>
2024-05-13 15:33:42 +0200

    SimpleLibrary dynamic libraries, along with caller class and tests.

---
+++ Assets/Emergence/EmergenceSDK/SimpleLibraryTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace EmergenceSDK
+{
+    public class SimpleLibraryTest
+    {
+        #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        [DllImport ("SimpleLibrary")]
+        private static extern int aotsample_add(int a, int b);
+        
+        [DllImport ("SimpleLibrary")]
+        private static extern string aotsample_sumstring(string a, string b);
+
+        public static int Add(int a, int b)
+        {
+            return aotsample_add(a, b);
+        }
+
+        public static string Concat(string a, string b)
+        {
+            return aotsample_sumstring(a, b);
+        }
+#endif
+    }
+}
---
+++ Assets/Emergence/EmergenceSDK/Tests/Plugins/SimpleLibrary/AssetRegistryTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using EmergenceSDK.Integrations.Futureverse.Internal;
+using EmergenceSDK.Integrations.Futureverse.Internal.Services;
+using EmergenceSDK.Integrations.Futureverse.Services;
+using EmergenceSDK.Integrations.Futureverse.Types;
+using EmergenceSDK.Types;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace EmergenceSDK.Tests.Plugins.SimpleLibrary
+{
+    [TestFixture]
+    public class SimpleLibraryTests
+    {
+        [Test]
+        public void SimpleLibrary_Sum_Success()
+        {
+            Assert.AreEqual(2, SimpleLibraryTest.Add(1, 1));
+            Assert.AreEqual(4, SimpleLibraryTest.Add(2, 2));
+            Assert.AreEqual(6, SimpleLibraryTest.Add(3, 3));
+        }
+
+        [Test]
+        public void SimpleLibrary_Concat_Success()
+        {
+            Assert.AreEqual("11", SimpleLibraryTest.Concat("1", "1"));
+            Assert.AreEqual("22", SimpleLibraryTest.Concat("2", "2"));
+            Assert.AreEqual("33", SimpleLibraryTest.Concat("3", "3"));
+        }
+    }
+}
