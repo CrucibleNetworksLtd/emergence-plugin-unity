2306c664e77e3d346145c0b9e878a8957b680af9
Guido Belluomo <156204138+CrucibleGuido@users.noreply.github.com>
2024-05-21 01:27:41 +0200

    Reordered functions based on visibility and importance

--- Assets/Emergence/EmergenceSDK/EmergenceServiceProvider.cs
+++ Assets/Emergence/EmergenceSDK/EmergenceServiceProvider.cs
@@ -21,30 +21,72 @@ namespace EmergenceSDK
         
         private static readonly List<IEmergenceService> Services = new();
         private static readonly ReaderWriterLockSlim Lock = new();
-        
-        private static void LoadFutureverseServices()
+
+        public static void Load(ServiceProfile profile)
         {
-            AddService(new AvatarService());
-            var sessionService = AddService(new SessionService());
-            var walletService = AddService(new WalletService(sessionService));
-            var futureverseService = AddService(new FutureverseService(walletService));
-            AddService(new FutureverseInventoryService(futureverseService));
-            AddService(new DynamicMetadataService());
-            AddService(new ContractService());
-            AddService(new ChainService());
-            AddService(new PersonaService(sessionService));
+            using (Lock.WriteLock())
+            {
+                Services.Clear();
+
+                if (profile == ServiceProfile.Default)
+                    LoadDefaultServices();
+                else if (profile == ServiceProfile.Futureverse)
+                    LoadFutureverseServices();
+                else
+                    throw new ArgumentOutOfRangeException(nameof(profile), profile, null);
+
+                LoadedProfile = profile;
+            }
+
+            OnServicesLoaded?.Invoke(profile);
         }
 
-        private static void LoadDefaultServices()
+        /// <summary>
+        /// Gets the service of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of service to request, must implement <see cref="IEmergenceService"/>.</typeparam>
+        /// <returns>The requested service or null if not found.</returns>
+        public static T GetService<T>() where T : class, IEmergenceService
         {
-            AddService(new AvatarService());
-            AddService(new InventoryService());
-            AddService(new DynamicMetadataService());
-            AddService(new ContractService());
-            AddService(new ChainService());
-            var sessionService = AddService(new SessionService());
-            AddService(new WalletService(sessionService));
-            AddService(new PersonaService(sessionService));
+            using IDisposable readLock = Lock.ReadLock();
+            foreach (var service in Services)
+            {
+                if (service is T typedService)
+                {
+                    return typedService;
+                }
+            }
+            return null;
+        }
+
+        public static void Unload()
+        {
+            using (Lock.WriteLock())
+            {
+                Services.Clear();
+                LoadedProfile = null;
+            }
+
+            OnServicesUnloaded?.Invoke();
+        }
+
+        /// <summary>
+        /// Gets all the services of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of service to request, must implement <see cref="IEmergenceService"/>.</typeparam>
+        /// <returns>A <see cref="List{T}"/> of the matching services.</returns>
+        internal static List<T> GetServices<T>() where T : class, IEmergenceService
+        {
+            using IDisposable readLock = Lock.ReadLock();
+            List<T> services = new List<T>();
+            foreach (var service in Services)
+            {
+                if (service is T typedService)
+                {
+                    services.Add(typedService);
+                }
+            }
+            return services;
         }
 
         private static T AddService<T>(T service) where T : class, IEmergenceService
@@ -66,71 +108,29 @@ namespace EmergenceSDK
             return service;
         }
 
-        /// <summary>
-        /// Gets the service of the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type of service to request, must implement <see cref="IEmergenceService"/>.</typeparam>
-        /// <returns>The requested service or null if not found.</returns>
-        public static T GetService<T>() where T : class, IEmergenceService
+        private static void LoadDefaultServices()
         {
-            using IDisposable readLock = Lock.ReadLock();
-            foreach (var service in Services)
-            {
-                if (service is T typedService)
-                {
-                    return typedService;
-                }
-            }
-            return null;
+            AddService(new AvatarService());
+            AddService(new InventoryService());
+            AddService(new DynamicMetadataService());
+            AddService(new ContractService());
+            AddService(new ChainService());
+            var sessionService = AddService(new SessionService());
+            AddService(new WalletService(sessionService));
+            AddService(new PersonaService(sessionService));
         }
 
-        /// <summary>
-        /// Gets all the services of the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type of service to request, must implement <see cref="IEmergenceService"/>.</typeparam>
-        /// <returns>A <see cref="List{T}"/> of the matching services.</returns>
-        public static List<T> GetServices<T>() where T : class, IEmergenceService
+        private static void LoadFutureverseServices()
         {
-            using IDisposable readLock = Lock.ReadLock();
-            List<T> services = new List<T>();
-            foreach (var service in Services)
-            {
-                if (service is T typedService)
-                {
-                    services.Add(typedService);
-                }
-            }
-            return services;
-        }
-
-        public static void Load(ServiceProfile profile)
-        {
-            using (Lock.WriteLock())
-            {
-                Services.Clear();
-
-                if (profile == ServiceProfile.Default)
-                    LoadDefaultServices();
-                else if (profile == ServiceProfile.Futureverse)
-                    LoadFutureverseServices();
-                else
-                    throw new ArgumentOutOfRangeException(nameof(profile), profile, null);
-
-                LoadedProfile = profile;
-            }
-
-            OnServicesLoaded?.Invoke(profile);
-        }
-
-        public static void Unload()
-        {
-            using (Lock.WriteLock())
-            {
-                Services.Clear();
-                LoadedProfile = null;
-            }
-
-            OnServicesUnloaded?.Invoke();
+            AddService(new AvatarService());
+            var sessionService = AddService(new SessionService());
+            var walletService = AddService(new WalletService(sessionService));
+            var futureverseService = AddService(new FutureverseService(walletService));
+            AddService(new FutureverseInventoryService(futureverseService));
+            AddService(new DynamicMetadataService());
+            AddService(new ContractService());
+            AddService(new ChainService());
+            AddService(new PersonaService(sessionService));
         }
     }
 
