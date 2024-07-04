ffda118a8f30913922724fe7127e60929f89d44a
Guido Belluomo <156204138+CrucibleGuido@users.noreply.github.com>
2024-07-04 17:17:17 +0200

    Disabled UniGLTF scripted importers and added gltfast

--- Assets/Emergence/Plugins/UniGLTF/Editor/UniGLTF/ScriptedImporter/GlbScriptedImporter.cs
+++ Assets/Emergence/Plugins/UniGLTF/Editor/UniGLTF/ScriptedImporter/GlbScriptedImporter.cs
@@ -7,7 +7,7 @@ using UnityEditor.Experimental.AssetImporters;
 
 namespace UniGLTF
 {
-#if UNITY_2020_2_OR_NEWER
+/*#if UNITY_2020_2_OR_NEWER
 #if UNIGLTF_DISABLE_DEFAULT_GLB_IMPORTER
     [ScriptedImporter(1, null, overrideExts: new[] { "glb" })]
 #else
@@ -15,7 +15,7 @@ namespace UniGLTF
 #endif
 #else
 	[ScriptedImporter(1, new[] { "glb" })]
-#endif
+#endif*/
     public class GlbScriptedImporter : GltfScriptedImporterBase
     {
         public override void OnImportAsset(AssetImportContext ctx)
--- Assets/Emergence/Plugins/UniGLTF/Editor/UniGLTF/ScriptedImporter/GltfScriptedImporter.cs
+++ Assets/Emergence/Plugins/UniGLTF/Editor/UniGLTF/ScriptedImporter/GltfScriptedImporter.cs
@@ -7,7 +7,7 @@ using UnityEditor.Experimental.AssetImporters;
 
 namespace UniGLTF
 {
-#if UNITY_2020_2_OR_NEWER
+/*#if UNITY_2020_2_OR_NEWER
 #if UNIGLTF_DISABLE_DEFAULT_GLTF_IMPORTER
     [ScriptedImporter(1, null, overrideExts: new[] { "gltf" })]
 #else
@@ -15,7 +15,7 @@ namespace UniGLTF
 #endif
 #else
 	[ScriptedImporter(1, new[] { "gltf" })]
-#endif
+#endif*/
     public class GltfScriptedImporter : GltfScriptedImporterBase
     {
         public override void OnImportAsset(AssetImportContext ctx)
